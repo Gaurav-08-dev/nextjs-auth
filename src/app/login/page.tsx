d4cb00108d3c102d33711f3480c7a6911d13cb8f
gaurav-08-dev <g8111997@gmail.com>
2023-08-18 23:37:55 +0530

    created basic structure for app - next will be working on sign up and login API

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,55 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
 export default function LoginPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const onLogin = async (e:React.SyntheticEvent) => {
+
+    e.stopPropagation();
+  };
+
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Login</h1>
+      <hr />
+    
+
+      <label htmlFor="email">Email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="email"
+        type="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="Email"
+      />
+
+      <label htmlFor="password">Password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="Password"
+      />
+
+      <button
+        type="submit"
+        className="p-2 border border-gray-300 rounded mb-4 focus:outline-none focus:border-gray-600"
+        onClick={onLogin}
+      >
+        Login
+      </button>
+      <Link href="/signup">Go to Signup</Link>
     </div>
   );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -12,14 +12,53 @@ export default function SignupPage() {
     username: "",
   });
 
-  const onSignUp = async () => {};
+  const onSignUp = async (e:React.SyntheticEvent) => {
+
+    e.stopPropagation();
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Signup</h1>
-      <hr/>
+      <hr />
       <label htmlFor="username">Username</label>
-      <input id="username" type="text"></input>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="username"
+        type="text"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="Username"
+      />
+
+      <label htmlFor="email">Email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="email"
+        type="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="Email"
+      />
+
+      <label htmlFor="password">Password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="Password"
+      />
+
+      <button
+        type="submit"
+        className="p-2 border border-gray-300 rounded mb-4 focus:outline-none focus:border-gray-600"
+        onClick={onSignUp}
+      >
+        Signup
+      </button>
+      <Link href="/login">Go to Login</Link>
     </div>
   );
 }
